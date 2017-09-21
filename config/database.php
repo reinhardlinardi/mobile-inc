b0839799017e07e8027d47686c99a9268c4e0a0e
Reinhard Linardi <reinhardlinardi@gmail.com>
2017-09-21 19:48:35 +0700

    Database connection test #2

--- config/database.php
+++ config/database.php
@@ -54,7 +54,10 @@ return [
             'host'     => parse_url(getenv("DATABASE_URL"))["host"],
             'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
             'username' => parse_url(getenv("DATABASE_URL"))["user"],
-            'password' => parse_url(getenv("DATABASE_URL"))["pass"]
+            'password' => parse_url(getenv("DATABASE_URL"))["pass"],
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'schema'   => 'public'
         ],
         
         /*
