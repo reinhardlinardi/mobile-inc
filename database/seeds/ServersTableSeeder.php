8a92dc0746ee389b7bb349a4f93e78c30e6a0f72
Reinhard Linardi <reinhardlinardi@gmail.com>
2017-09-30 12:29:41 +0700

    Add server key seeder

---
+++ database/seeds/ServersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('servers')->insert([
+            'server_key' => env('SERVER_KEY'),
+            'created_at' => Carbon\Carbon::now(),
+            'updated_at' => Carbon\Carbon::now()
+        ]);
+    }
+}
